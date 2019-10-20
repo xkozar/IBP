f30a1bd6845dea13a0591fd043ae44b7585165fb
tomas.kozar <tomas.kozar@solarwinds.com>
2019-10-20 22:12:19 +0200

    CYK for BKG

---
+++ parser.py
@@ -0,0 +1,72 @@
+from ruleReader import RuleReader
+from pprint import pprint
+
+def printTable(tab):
+    temp = table.copy()
+    temp.reverse()
+    for row in temp:
+        pprint(row)
+
+word = 'bcbc'
+modified = True # Determines whether rules table was modified
+
+reader = RuleReader("rules.txt")
+rules = reader.contentToPairs()
+# Context free grammar
+table = [['' for i in range(word.__len__())] for j in range(word.__len__())]
+
+def findRule(rightSide):
+    result = ''
+    global modified
+    for leftSide in rules:
+        if rightSide in rules[leftSide]:
+            result = result + leftSide
+    
+    return(result)
+
+for i in range(word.__len__()):
+    for lSide in rules:
+        if word[i] in rules[lSide]:
+            table[i][i] = table[i][i] + lSide
+
+
+def findPairForRule(row, column, nTerminal):
+    global modified
+    if column == word.__len__()-1:
+        return
+
+    for col, tRules in enumerate(table[column+1]):
+        if tRules == '':
+            continue
+        for nTerm in tRules:
+            result = findRule(nTerminal + nTerm)
+            # if result == '':
+            #     continue
+            for character in result:
+                if table[row][col].find(character) < 0:
+                    table[row][col] = table[row][col] + character
+                    modified = True
+
+
+            
+
+while modified:
+    modified = False
+    printTable(table)
+    print('-----------------------------------------')
+    for idr, row in enumerate(table):
+        for idc, tableRules in enumerate(row):
+            if(tableRules is not ''):
+                for nonTerminal in tableRules:
+                    findPairForRule(idr, idc, nonTerminal)
+
+    print('end of loop')
+
+
+
+printTable(table)
+if table[0][word.__len__()-1].find('S') >= 0:
+    print("NICE")
+else:
+    print("NOPE")
+
---
+++ ruleReader.py
@@ -0,0 +1,33 @@
+import re
+
+class RuleReader:
+    
+    def __init__(self, name):
+        self.filename = name
+        self.content = []
+        self.contentPairs = []
+        self.getFileContent()
+        self.rulesDictionary = {}
+
+    def getFileContent(self):
+        with open(self.filename, 'r') as file:
+            self.content = file.read()
+
+    def contentToPairs(self):
+        # TODO use regex to check if rule is in proper format!!!
+        for line in self.content.split('\n'):
+            # Remove empty spaces from file
+            if line == '':
+                continue
+
+            # Remove whitespace and split
+            rulePair = ''.join(line.split()).split('->')
+            if rulePair[0] in self.rulesDictionary:
+                self.rulesDictionary[rulePair[0]].append(rulePair[1])
+            else:
+                self.rulesDictionary[rulePair[0]] = [rulePair[1]]
+        return self.rulesDictionary   
+
+    def getRulesDictionary(self):
+        return 0
+        
